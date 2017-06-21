43b22c8213a6e24ffa97098cad9da52a077c9f13
Andrew Sealy <andrewjsealy@live.ca>
2017-06-21 15:46:53 -0600

    Add files via upload

--- threads.cpp
+++ threads.cpp
@@ -5,17 +5,52 @@
 #include <fstream>
 using namespace std;
 
+char sudoku_array [9][9]={0};
+int all_threads_valid[11]={0};
+
 struct parameters
 {
    int row;
    int column;
 };
 
-void *threadFunction(void *params) {
+void *RowChecker(void *params) {
+   int is_valid=1;
    struct parameters *stucture;
    stucture = (struct parameters *) params;
-   cout << "Row: " << stucture->row;
-   cout << "Column: " << stucture->column;
+	for(int i=0;i<9;i++){
+	int columnsArray[9]={0};
+		for(int j=0;j<9;j++){
+			columnsArray[(int)sudoku_array[i][j]-49]=1;
+				cout << sudoku_array[i][j];		
+		}
+		for(int j=0;j<9;j++){
+			cout << columnsArray[j];
+			if(columnsArray[j]==0) is_valid=0;
+		}
+	
+	}
+	if(is_valid==1) all_threads_valid[0]=1;
+   pthread_exit(NULL);
+}
+
+void *ColumnChecker(void *params) {
+   int is_valid=1;
+   struct parameters *stucture;
+   stucture = (struct parameters *) params;
+	for(int i=0;i<9;i++){
+	int columnsArray[9]={0};
+		for(int j=0;j<9;j++){
+			columnsArray[(int)sudoku_array[j][i]-49]=1;
+				cout << sudoku_array[j][i];		
+		}
+		for(int j=0;j<9;j++){
+			cout << columnsArray[j];
+			if(columnsArray[j]==0) is_valid=0;
+		}
+	
+	}
+	if(is_valid==1) all_threads_valid[0]=1;
    pthread_exit(NULL);
 }
 
@@ -25,22 +60,24 @@ int main (int argc, char** argv) {
    std::ifstream stream("sudoku_solution.txt");
    std::string sudoku_string((std::istreambuf_iterator<char>(stream)),(std::istreambuf_iterator<char>()));
 
-   cout << sudoku_string << "\n";
+   //cout << sudoku_string << "\n";
 
-   char sudoku_array [9][9]={0};
+   
    int count=0;
    for(int i=0;i<9;i++){
       for(int j=0;j<9;j++){
          sudoku_array[i][j]=sudoku_string[count];
          count=count+2;
-      }
+      }count++;
    }
 
+	cout << endl;
    for(int i=0;i<9;i++){
       for(int j=0;j<9;j++){
          cout << sudoku_array[i][j];
       }cout << "\n";
    }
+cout << endl;
 
 
 
@@ -48,19 +85,27 @@ int main (int argc, char** argv) {
 
 
 
-
-   pthread_t thread;
-   struct parameters td;
-   td.row = 1;
-   td.column = 1;
-   int rc = pthread_create(&thread, NULL, threadFunction, (void *)&td);
-
+   pthread_t thread_1;
+   struct parameters p_1;
+   p_1.row = 0;
+   p_1.column = 0;
+   int rc = pthread_create(&thread_1, NULL, RowChecker, (void *)&p_1);
 
 
-
-
-
+    pthread_t thread_2;
+   struct parameters p_2;
+   p_2.row = 0;
+   p_2.column = 0;
+   rc = pthread_create(&thread_2, NULL, ColumnChecker, (void *)&p_2);
 
 
    pthread_exit(NULL);
+
+
+
+
+
+
+
+   
 }
