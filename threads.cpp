f42d17b10f9bed7b5937705f1322af9e9159854a
Andrew Sealy <andrewjsealy@live.ca>
2017-06-23 16:07:46 -0600

    Add files via upload

--- threads.cpp
+++ threads.cpp
@@ -5,316 +5,264 @@
 #include <fstream>
 using namespace std;
 
+//this is the char array for the sudoku board, it is char because it was read from the text file as a string 
 char sudoku_array [9][9]={0};
+//this is the array to check if each region is valid 
 static int all_threads_valid[11]={0};
 
-
-
-
+//these coordinates parameters are passed to the threads
 struct parameters
 {
    int row;
    int column;
 };
 
+//this is the row checker thread that checks if each row is valid 
+//this will be the only thread that is commented as the others follow the same implementation
 void *RowChecker(void *params) {
+   //this int is to see if the region is valid 
    int is_valid=1;
+   //a new struct is dynamically initialized according to the parameters 
    struct parameters *structure;
    structure = (struct parameters *) params;
 
-	for(int i=0;i<9;i++){
-	int columnsArray[9]={0};
-		for(int j=0;j<9;j++){
-			columnsArray[(int)sudoku_array[i][j]-49]=1;
-				//cout << sudoku_array[i][j];		
-		}
-		for(int j=0;j<9;j++){
-			//cout << columnsArray[j];
-			if(columnsArray[j]==0) is_valid=0;
-		}
-	
-	}
-	if(is_valid==1) all_threads_valid[0]=1;
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
-	
+   //each row is then checked 
+   for(int i=0;i<9;i++){
+      int columnsArray[9]={0};
+      for(int j=0;j<9;j++){
+         //the value of the on the sukudo board is used as an index to set the columnsArray index to 1 if that number is present
+         columnsArray[(int)sudoku_array[i][j]-49]=1;
+      }
+      //then the columnsArray is checked to see if it is all 1s. if it is all 1s it is valid sudoku region 
+      for(int j=0;j<9;j++){
+         if(columnsArray[j]==0) is_valid=0;
+      }
+   }
+   //if this region is valid, all_threads_valid[0] is set to 1
+   if(is_valid==1) all_threads_valid[0]=1;
 
-	int valid_sudoku=0;
-	for(int i=0;i<10;i++){
-		if(all_threads_valid[i]==0) valid_sudoku=1;
-	}
-	if(valid_sudoku==0) cout << "This sudoku solution is valid!" <<endl;
-	else cout << "This sudoku solution is NOT valid!" <<endl;
-
-	
-
-
-
-   //pthread_exit(NULL);
+   //this is the last thread to run for some reason so we can check the entire all_threads_valid array to see if all other regions are valid 
+   int valid_sudoku=0;
+   for(int i=0;i<10;i++){
+      if(all_threads_valid[i]==0) valid_sudoku=1;
+   }
+   //if the other regions are valid, print a valid message, otherwise print an invalid message 
+   if(valid_sudoku==0) cout << "This sudoku solution is valid!" <<endl;
+   else cout << "This sudoku solution is NOT valid!" <<endl;
 }
+
+
+
 
 void *ColumnChecker(void *params) {
    int is_valid=1;
    struct parameters *structure;
    structure = (struct parameters *) params;
 
-	for(int i=0;i<9;i++){
-	int rowsArray[9]={0};
-		for(int j=0;j<9;j++){
-			rowsArray[(int)sudoku_array[j][i]-49]=1;
-				//cout << sudoku_array[j][i];		
-		}
-		for(int j=0;j<9;j++){
-			//cout << columnsArray[j];
-			if(rowsArray[j]==0) is_valid=0;
-		}
-	
-	}
-	if(is_valid==1) all_threads_valid[1]=1;
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
-   //pthread_exit(NULL);
+   for(int i=0;i<9;i++){
+      int rowsArray[9]={0};
+      for(int j=0;j<9;j++){
+         rowsArray[(int)sudoku_array[j][i]-49]=1;
+      }
+      for(int j=0;j<9;j++){
+         if(rowsArray[j]==0) is_valid=0;
+      }
+
+   }
+   if(is_valid==1) all_threads_valid[1]=1;
 }
 
 void *ThreeByThreeChecker1(void *params) {
-   	int is_valid=1;
+   int is_valid=1;
 
-   	struct parameters *structure;
-   	structure = (struct parameters *) params;
+   struct parameters *structure;
+   structure = (struct parameters *) params;
 
-	int gridArray[9]={0};
-	for(int i=0;i<3;i++){
-      		for(int j=0;j<3;j++){
-			gridArray[(int)sudoku_array[i][j]-49]=1;
-			cout << sudoku_array[i][j];			
-		}
-	}
+   int gridArray[9]={0};
+   for(int i=0;i<3;i++){
+      for(int j=0;j<3;j++){
+         gridArray[(int)sudoku_array[i][j]-49]=1;
+      }
+   }
 
-	for(int i=0;i<9;i++){
-		if(gridArray[i]==0) is_valid=0;
-	}
-		
-	if(is_valid==1) all_threads_valid[2]=1;
+   for(int i=0;i<9;i++){
+      if(gridArray[i]==0) is_valid=0;
+   }
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   if(is_valid==1) all_threads_valid[2]=1;
 }
 
 void *ThreeByThreeChecker2(void *params) {
-   	int is_valid=1;
+   int is_valid=1;
 
-   	struct parameters *structure;
-   	structure = (struct parameters *) params;
+   struct parameters *structure;
+   structure = (struct parameters *) params;
 
-	cout <<endl<< "coordinates:" <<structure->row << structure->column<<endl;
-	int gridArray[9]={0};
-	for(int i=3;i<6;i++){
-      		for(int j=0;j<3;j++){
-			gridArray[(int)sudoku_array[i][j]-49]=1;
-			cout << sudoku_array[i][j];			
-		}
-	}
+   int gridArray[9]={0};
+   for(int i=3;i<6;i++){
+      for(int j=0;j<3;j++){
+         gridArray[(int)sudoku_array[i][j]-49]=1;
+      }
+   }
 
-	for(int i=0;i<9;i++){
-		if(gridArray[i]==0) is_valid=0;
-	}
-		
-	if(is_valid==1) all_threads_valid[3]=1;
+   for(int i=0;i<9;i++){
+      if(gridArray[i]==0) is_valid=0;
+   }
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   if(is_valid==1) all_threads_valid[3]=1;
 }
+
 void *ThreeByThreeChecker3(void *params) {
-   	int is_valid=1;
+   int is_valid=1;
 
-   	struct parameters *structure;
-   	structure = (struct parameters *) params;
+   struct parameters *structure;
+   structure = (struct parameters *) params;
 
-	cout <<endl<< "coordinates:" <<structure->row << structure->column<<endl;
-	int gridArray[9]={0};
-	for(int i=6;i<9;i++){
-      		for(int j=0;j<3;j++){
-			gridArray[(int)sudoku_array[i][j]-49]=1;
-			cout << sudoku_array[i][j];			
-		}
-	}
+   int gridArray[9]={0};
+   for(int i=6;i<9;i++){
+      for(int j=0;j<3;j++){
+         gridArray[(int)sudoku_array[i][j]-49]=1;
+      }
+   }
 
-	for(int i=0;i<9;i++){
-		if(gridArray[i]==0) is_valid=0;
-	}
-		
-	if(is_valid==1) all_threads_valid[4]=1;
+   for(int i=0;i<9;i++){
+      if(gridArray[i]==0) is_valid=0;
+   }
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   if(is_valid==1) all_threads_valid[4]=1;
 }
+
 void *ThreeByThreeChecker4(void *params) {
-   	int is_valid=1;
+   int is_valid=1;
 
-   	struct parameters *structure;
-   	structure = (struct parameters *) params;
+   struct parameters *structure;
+   structure = (struct parameters *) params;
 
-	cout <<endl<< "coordinates:" <<structure->row << structure->column<<endl;
-	int gridArray[9]={0};
-	for(int i=0;i<3;i++){
-      		for(int j=3;j<6;j++){
-			gridArray[(int)sudoku_array[i][j]-49]=1;
-			cout << sudoku_array[i][j];			
-		}
-	}
+   int gridArray[9]={0};
+   for(int i=0;i<3;i++){
+      for(int j=3;j<6;j++){
+         gridArray[(int)sudoku_array[i][j]-49]=1;
+      }
+   }
 
-	for(int i=0;i<9;i++){
-		if(gridArray[i]==0) is_valid=0;
-	}
-		
-	if(is_valid==1) all_threads_valid[5]=1;
+   for(int i=0;i<9;i++){
+      if(gridArray[i]==0) is_valid=0;
+   }
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   if(is_valid==1) all_threads_valid[5]=1;
 }
+
 void *ThreeByThreeChecker5(void *params) {
-   	int is_valid=1;
+   int is_valid=1;
 
-   	struct parameters *structure;
-   	structure = (struct parameters *) params;
+   struct parameters *structure;
+   structure = (struct parameters *) params;
 
-	cout <<endl<< "coordinates:" <<structure->row << structure->column<<endl;
-	int gridArray[9]={0};
-	for(int i=3;i<6;i++){
-      		for(int j=3;j<6;j++){
-			gridArray[(int)sudoku_array[i][j]-49]=1;
-			cout << sudoku_array[i][j];			
-		}
-	}
+   int gridArray[9]={0};
+   for(int i=3;i<6;i++){
+      for(int j=3;j<6;j++){
+         gridArray[(int)sudoku_array[i][j]-49]=1;
+      }
+   }
 
-	for(int i=0;i<9;i++){
-		if(gridArray[i]==0) is_valid=0;
-	}
-		
-	if(is_valid==1) all_threads_valid[6]=1;
+   for(int i=0;i<9;i++){
+      if(gridArray[i]==0) is_valid=0;
+   }
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   if(is_valid==1) all_threads_valid[6]=1;
 }
+
 void *ThreeByThreeChecker6(void *params) {
-   	int is_valid=1;
+   int is_valid=1;
 
-   	struct parameters *structure;
-   	structure = (struct parameters *) params;
+   struct parameters *structure;
+   structure = (struct parameters *) params;
 
-	cout <<endl<< "coordinates:" <<structure->row << structure->column<<endl;
-	int gridArray[9]={0};
-	for(int i=6;i<9;i++){
-      		for(int j=3;j<6;j++){
-			gridArray[(int)sudoku_array[i][j]-49]=1;
-			cout << sudoku_array[i][j];			
-		}
-	}
+   int gridArray[9]={0};
+   for(int i=6;i<9;i++){
+      for(int j=3;j<6;j++){
+         gridArray[(int)sudoku_array[i][j]-49]=1;
+      }
+   }
 
-	for(int i=0;i<9;i++){
-		if(gridArray[i]==0) is_valid=0;
-	}
-		
-	if(is_valid==1) all_threads_valid[7]=1;
+   for(int i=0;i<9;i++){
+      if(gridArray[i]==0) is_valid=0;
+   }
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   if(is_valid==1) all_threads_valid[7]=1;
 }
+
 void *ThreeByThreeChecker7(void *params) {
-   	int is_valid=1;
+   int is_valid=1;
 
-   	struct parameters *structure;
-   	structure = (struct parameters *) params;
+   struct parameters *structure;
+   structure = (struct parameters *) params;
 
-	cout <<endl<< "coordinates:" <<structure->row << structure->column<<endl;
-	int gridArray[9]={0};
-	for(int i=0;i<3;i++){
-      		for(int j=6;j<9;j++){
-			gridArray[(int)sudoku_array[i][j]-49]=1;
-			cout << sudoku_array[i][j];			
-		}
-	}
+   int gridArray[9]={0};
+   for(int i=0;i<3;i++){
+      for(int j=6;j<9;j++){
+         gridArray[(int)sudoku_array[i][j]-49]=1;
+      }
+   }
 
-	for(int i=0;i<9;i++){
-		if(gridArray[i]==0) is_valid=0;
-	}
-		
-	if(is_valid==1) all_threads_valid[8]=1;
+   for(int i=0;i<9;i++){
+      if(gridArray[i]==0) is_valid=0;
+   }
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   if(is_valid==1) all_threads_valid[8]=1;
 }
+
 void *ThreeByThreeChecker8(void *params) {
-   	int is_valid=1;
+   int is_valid=1;
 
-   	struct parameters *structure;
-   	structure = (struct parameters *) params;
+   struct parameters *structure;
+   structure = (struct parameters *) params;
 
-	cout <<endl<< "coordinates:" <<structure->row << structure->column<<endl;
-	int gridArray[9]={0};
-	for(int i=3;i<6;i++){
-      		for(int j=6;j<9;j++){
-			gridArray[(int)sudoku_array[i][j]-49]=1;
-			cout << sudoku_array[i][j];			
-		}
-	}
+   int gridArray[9]={0};
+   for(int i=3;i<6;i++){
+      for(int j=6;j<9;j++){
+         gridArray[(int)sudoku_array[i][j]-49]=1;
+      }
+   }
 
-	for(int i=0;i<9;i++){
-		if(gridArray[i]==0) is_valid=0;
-	}
-		
-	if(is_valid==1) all_threads_valid[9]=1;
+   for(int i=0;i<9;i++){
+      if(gridArray[i]==0) is_valid=0;
+   }
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   if(is_valid==1) all_threads_valid[9]=1;
 }
+
 void *ThreeByThreeChecker9(void *params) {
-   	int is_valid=1;
+   int is_valid=1;
 
-   	struct parameters *structure;
-   	structure = (struct parameters *) params;
+   struct parameters *structure;
+   structure = (struct parameters *) params;
 
-	cout <<endl<< "coordinates:" <<structure->row << structure->column<<endl;
-	int gridArray[9]={0};
-	for(int i=6;i<9;i++){
-      		for(int j=6;j<9;j++){
-			gridArray[(int)sudoku_array[i][j]-49]=1;
-			cout << sudoku_array[i][j];			
-		}
-	}
+   int gridArray[9]={0};
+   for(int i=6;i<9;i++){
+      for(int j=6;j<9;j++){
+         gridArray[(int)sudoku_array[i][j]-49]=1;
+      }
+   }
 
-	for(int i=0;i<9;i++){
-		if(gridArray[i]==0) is_valid=0;
-	}
-		
-	if(is_valid==1) all_threads_valid[10]=1;
+   for(int i=0;i<9;i++){
+      if(gridArray[i]==0) is_valid=0;
+   }
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   if(is_valid==1) all_threads_valid[10]=1;
 }
 
 
 
 
 
+
+//main functions 
 int main (int argc, char** argv) {
+
+   //this is an implementation for converting a text file to a C++ string 
    std::ifstream stream("sudoku_solution.txt");
    std::string sudoku_string((std::istreambuf_iterator<char>(stream)),(std::istreambuf_iterator<char>()));
-
-   //cout << sudoku_string << "\n";
-
-   
    int count=0;
    for(int i=0;i<9;i++){
       for(int j=0;j<9;j++){
@@ -323,72 +271,67 @@ int main (int argc, char** argv) {
       }count++;
    }
 
-	cout << endl;
-   for(int i=0;i<9;i++){
-      for(int j=0;j<9;j++){
-         cout << sudoku_array[i][j];
-      }cout << "\n";
-   }
-
-
-
-
-   pthread_t thread;
+   //this is where the threads are created
+   // as per the instructions, i made 2 threads for row and columns and 9 threads for the grid regions 
+   pthread_t thread1;
+   //this is struct parameters which is used again for each thread, it gets updated for the grid regions 
    struct parameters p;
    p.row = 0;
    p.column = 0;
-   int rc = pthread_create(&thread, NULL, RowChecker, (void *)&p);
+   //pthread_create is called with the specific thread and parameters 
+   int rc = pthread_create(&thread1, NULL, RowChecker, (void *)&p);
 
+   pthread_t thread2;
    p.row = 0;
    p.column = 0;
-   rc = pthread_create(&thread, NULL, ColumnChecker, (void *)&p);
+   rc = pthread_create(&thread2, NULL, ColumnChecker, (void *)&p);
 
-p.row = 0;
-p.column = 0;
-rc = pthread_create(&thread, NULL, ThreeByThreeChecker1, (void *)&p);
-p.row = 3;
-p.column = 0;
-rc = pthread_create(&thread, NULL, ThreeByThreeChecker2, (void *)&p);
-p.row = 6;
-p.column = 0;
-rc = pthread_create(&thread, NULL, ThreeByThreeChecker3, (void *)&p);
-p.row = 0;
-p.column = 3;
-rc = pthread_create(&thread, NULL, ThreeByThreeChecker4, (void *)&p);
-p.row = 3;
-p.column = 3;
-rc = pthread_create(&thread, NULL, ThreeByThreeChecker5, (void *)&p);
-p.row = 6;
-p.column = 3;
-rc = pthread_create(&thread, NULL, ThreeByThreeChecker6, (void *)&p);
-p.row = 0;
-p.column = 6;
-rc = pthread_create(&thread, NULL, ThreeByThreeChecker7, (void *)&p);
-p.row = 3;
-p.column = 6;
-rc = pthread_create(&thread, NULL, ThreeByThreeChecker8, (void *)&p);
-p.row = 6;
-p.column = 6;
-rc = pthread_create(&thread, NULL, ThreeByThreeChecker9, (void *)&p);
+   pthread_t thread3;
+   p.row = 0;
+   p.column = 0;
+   rc = pthread_create(&thread3, NULL, ThreeByThreeChecker1, (void *)&p);
 
-	
+   pthread_t thread4;
+   p.row = 3;
+   p.column = 0;
+   rc = pthread_create(&thread4, NULL, ThreeByThreeChecker2, (void *)&p);
 
-	cout << endl;
-	for(int i=0;i<11;i++) cout << all_threads_valid[i];
-	cout << endl;
+   pthread_t thread5;
+   p.row = 6;
+   p.column = 0;
+   rc = pthread_create(&thread5, NULL, ThreeByThreeChecker3, (void *)&p);
 
+   pthread_t thread6;
+   p.row = 0;
+   p.column = 3;
+   rc = pthread_create(&thread6, NULL, ThreeByThreeChecker4, (void *)&p);
 
-   
-pthread_exit(NULL);
-return 0;
+   pthread_t thread7;
+   p.row = 3;
+   p.column = 3;
+   rc = pthread_create(&thread7, NULL, ThreeByThreeChecker5, (void *)&p);
 
+   pthread_t thread8;
+   p.row = 6;
+   p.column = 3;
+   rc = pthread_create(&thread8, NULL, ThreeByThreeChecker6, (void *)&p);
 
+   pthread_t thread9;
+   p.row = 0;
+   p.column = 6;
+   rc = pthread_create(&thread9, NULL, ThreeByThreeChecker7, (void *)&p);
 
+   pthread_t thread10;
+   p.row = 3;
+   p.column = 6;
+   rc = pthread_create(&thread10, NULL, ThreeByThreeChecker8, (void *)&p);
 
-   
+   pthread_t thread11;
+   p.row = 6;
+   p.column = 6;
+   rc = pthread_create(&thread11, NULL, ThreeByThreeChecker9, (void *)&p);
+
+   pthread_exit(NULL);
+   return 0;
+
 }
-
-
-
-
-
